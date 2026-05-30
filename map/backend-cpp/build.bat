b9918a8f66733338b92121621a0d43c062120c5f
Jay-118-code <2229532463@qq.com>
2026-05-30 13:02:36 +0800

    chore: 删除冗余文件
    - 删除 backend-cpp/ 目录(C++后端,与Express重复)
    - 删除 public/vite.svg(Vite默认占位图标)

--- map/backend-cpp/build.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat" >nul 2>&1
-cmake -S . -B build -G "NMake Makefiles" && cmake --build build
-copy /Y "C:\Program Files\MySQL\MySQL Server 8.0\lib\libmysql.dll" build\ >nul
-echo.
-echo Build complete. Run: build\server.exe
