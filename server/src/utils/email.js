818bf77e3c45a586f2b5033e26c15f3e36766122
Jay-118-code <2229532463@qq.com>
2026-06-10 13:57:22 +0800

    强制 SMTP 使用 IPv4 连接，修复 Railway IPv6 不可达

--- server/src/utils/email.js
+++ server/src/utils/email.js
@@ -9,6 +9,8 @@ function getTransporter() {
     connectionTimeout: 10000,
     greetingTimeout: 10000,
     socketTimeout: 10000,
+    tls: { ciphers: 'SSLv3' },
+    family: 4,
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
